a9cf5a33b15e54cfa424948ef94ee1d6dda692fa
Rodrigo Galdino <63233782+rodrigogaldino553@users.noreply.github.com>
2020-10-10 11:36:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,27 @@
 var now = 0
-
-/*function loadPhotos() {
-    var field = document.querySelector('.slider')
-
-    for (let c = 0; c < 12; c++) {
-        field.innerHTML += `<li id="${c}" value="${c}">
-        /assets/${c}.jpg
-    </li>`
-    }
-    console.log(field)
-    
-}*/
 let img = document.getElementById('view')
+
+
 function slide(mod) {
-    if (now < 0) {
-        now = 8
-    }else if(now > 11){
-        now = 3
-    }
-
-    switch (mod) {
-        case 1:
+    if (mod == 1) {
+        if (now >= 11) {
+            now = 0
+        } else {
             now++
-            break;
-        
-        case 2:
-            now --
-            break;
-        default:
+        }
+    } else {
+        if (now <= 0) {
             now = 11
-            break;
+        } else {
+            now--
+        }
     }
 
-    console.log(now)
     img.setAttribute('src', `/assets/${now}.jpg`)
-    /*if (mod == 'Next') {
-        now++
-        console.log(now)
-        img.setAttribute('src', `/assets/${now}.jpg`)
-
-    } else {
-        now--
-
-    }*/
+    if (now <= 0) {
+        now = 11
+    } else if (now >= 11) {
+        now = 0
+    }
 }
-
-//loadPhotos()
 
